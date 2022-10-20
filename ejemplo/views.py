b1cf2cab49fb21a56a46e8f89f3f1239b702ec86
marinalgarcia <marinalgarcia@gmail.com>
2022-10-19 23:28:08 -0300

    segundo commit de mi repositorio 2

--- ejemplo/views.py
+++ ejemplo/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.shortcuts import render
+from ejemplo.models import Familiar
 
 def index(request):
     return render(request, "ejemplo/saludar.html")
+    
+def monstrar_familiares(request):
+  lista_familiares = Familiar.objects.all()
+  return render(request, "ejemplo/familiares.html", {"lista_familiares": lista_familiares})
---
+++ seed_data.py
@@ -0,0 +1,10 @@
+from ejemplo.models import Familiar
+
+#Familiar(nombre="Julian", direccion="Rio de Janeiro 700", numero_pasaporte=123123).save()
+#Familiar(nombre="Sofia", direccion="Rio de Janeiro 700", numero_pasaporte=890890).save()
+#Familiar(nombre="Esteban", direccion="Maure 4000", numero_pasaporte=345345).save()
+#Familiar(nombre="Maria", direccion="Martinez 234", numero_pasaporte=567567).save()
+Familiar(nombre="Lucas", direccion="Avenida Rivadavia 7002", numero_pasaporte=239098).save()
+Familiar(nombre="Florencia", direccion="Angel Gallardo 2034", numero_pasaporte=193503).save()
+
+print("Se cargo con éxito los usuarios de pruebas")
