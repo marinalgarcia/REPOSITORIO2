8febc10832af5c5c547c89629e034e0cea4f430f
marinalgarcia <marinalgarcia@gmail.com>
2022-10-23 17:52:50 -0300

    nuevos cambioss clase 21o

--- ejemplo/views.py
+++ ejemplo/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 from django.shortcuts import render
 from ejemplo.models import Familiar
+from ejemplo.forms import Buscar, FamiliarForm # <--- NUEVO IMPORT
+from django.views import View # <-- NUEVO IMPORT 
 
 def index(request):
     return render(request, "ejemplo/saludar.html")
@@ -10,3 +12,44 @@ def index(request):
 def monstrar_familiares(request):
   lista_familiares = Familiar.objects.all()
   return render(request, "ejemplo/familiares.html", {"lista_familiares": lista_familiares})
+
+class BuscarFamiliar(View):
+
+    form_class = Buscar
+    template_name = 'ejemplo/buscar.html'
+    initial = {"nombre":""}
+
+    def get(self, request):
+        form = self.form_class(initial=self.initial)
+        return render(request, self.template_name, {'form':form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            nombre = form.cleaned_data.get("nombre")
+            lista_familiares = Familiar.objects.filter(nombre__icontains=nombre).all() 
+            form = self.form_class(initial=self.initial)
+            return render(request, self.template_name, {'form':form, 
+                                                        'lista_familiares':lista_familiares})
+        return render(request, self.template_name, {"form": form})
+
+class AltaFamiliar(View):
+
+    form_class = FamiliarForm
+    template_name = 'ejemplo/alta_familiar.html'
+    initial = {"nombre":"", "direccion":"", "numero_pasaporte":""}
+
+    def get(self, request):
+        form = self.form_class(initial=self.initial)
+        return render(request, self.template_name, {'form':form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            form.save()
+            msg_exito = f"se cargo con éxito el familiar {form.cleaned_data.get('nombre')}"
+            form = self.form_class(initial=self.initial)
+            return render(request, self.template_name, {'form':form, 
+                                                        'msg_exito': msg_exito})
+        
+        return render(request, self.template_name, {"form": form})
--- project/urls.py
+++ project/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from ejemplo.views import index, monstrar_familiares
+from ejemplo.views import index, monstrar_familiares, BuscarFamiliar, AltaFamiliar
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('saludar/', index), # ESTA ES LA NUEVA FUNCTION
     path('mi-familia/', monstrar_familiares), # nueva vista
+    path('mi-familia/buscar', BuscarFamiliar.as_view()), # NUEVA RUTA PARA BUSCAR FAMILIAR
+    path('mi-familia/alta', AltaFamiliar.as_view()) # NUEVA RUTA PARA BUSCAR FAMILIAR
 ]
