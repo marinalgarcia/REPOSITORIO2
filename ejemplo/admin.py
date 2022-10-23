c96cdcef72b77819d0de9a69b66e0f3164bd4b4d
marinalgarcia <marinalgarcia@gmail.com>
2022-10-23 19:43:07 -0300

    se agrega la aplicacion blog y un template avanzado

--- ejemplo/admin.py
+++ ejemplo/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from ejemplo.models import Familiar
+
+admin.site.register(Familiar)
--- project/urls.py
+++ project/urls.py
@@ -16,11 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from ejemplo.views import index, monstrar_familiares, BuscarFamiliar, AltaFamiliar
+from blog.views import index as blog_index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('saludar/', index), # ESTA ES LA NUEVA FUNCTION
     path('mi-familia/', monstrar_familiares), # nueva vista
     path('mi-familia/buscar', BuscarFamiliar.as_view()), # NUEVA RUTA PARA BUSCAR FAMILIAR
-    path('mi-familia/alta', AltaFamiliar.as_view()) # NUEVA RUTA PARA BUSCAR FAMILIAR
+    path('mi-familia/alta', AltaFamiliar.as_view()), # NUEVA RUTA PARA BUSCAR FAMILIAR
+    path('blog/', blog_index),
 ]
